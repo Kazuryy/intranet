ccf9a05516ba58e1353892dc1573d809d037f006
Kazuryy <luc.jacques72@gmail.com>
2026-03-30 16:52:40 +0200

    fix: update conftest with app context and db setup

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,16 @@
 import pytest
 from app import create_app
+from app.models import db
 
 
 @pytest.fixture
 def app():
-    app = create_app()
-    app.config["TESTING"] = True
-    yield app
+    app = create_app({'TESTING': True, 'SQLALCHEMY_DATABASE_URI': 'sqlite:///:memory:'})
+
+    with app.app_context():
+        db.create_all()
+        yield app
+        db.drop_all()
 
 
 @pytest.fixture
