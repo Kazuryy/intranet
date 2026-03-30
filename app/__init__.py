fa15d264626f80897e60c473861f21fbaf50fb0b
Kazuryy <luc.jacques72@gmail.com>
2026-03-30 16:52:33 +0200

    feat: initialize flask app factory with db and login manager

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,29 @@
+import os
 from flask import Flask, jsonify
+from flask_login import LoginManager
+from .models import db
 
 
-def create_app():
+def create_app(config=None):
     app = Flask(__name__)
+
+    # COnfig
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+        'DATABASE_URL', 'sqlite:///:memory:'
+        )
+    app.config['SECRET_KEY'] = 'dev_secret_key'
+
+    if config:
+        app.config.update(config)
+
+    db.init_app(app)
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        from .models import User
+        return User.query.get(int(user_id))
 
     @app.route("/health")
     def health():
